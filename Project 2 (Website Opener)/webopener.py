13c17e2a905667473cff5ed3950be5b2134c4852
Prajesh Gaikwad <102733514+prajesh8484@users.noreply.github.com>
2023-08-20 16:23:30 +0530

    Update webopener.py
    added comments and  some minor improvements in code

--- Project 2 (Website Opener)/webopener.py
+++ Project 2 (Website Opener)/webopener.py
@@ -4,54 +4,57 @@ import os
 from termcolor import colored
 import sys
 
-os.system('color')
-os.system('cls')
+# Clear the terminal screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def runloop():
-    codn = True
-    while codn == True:
-        header()
-        app()
-        exzt()
-
+# Function to display the header/banner
 def header():
-    print('-'*70)
+    print('-' * 70)
     banner = pyfiglet.figlet_format("Site Opener").upper()
-    print((colored(banner.rstrip("\n"), 'red', attrs=['bold'])))
-    print((colored("     -by prajesh8484     \n", 'yellow', attrs=['bold'])))
-    print('-'*70)
+    print(colored(banner.rstrip("\n"), 'red', attrs=['bold']))
+    print(colored("     -by prajesh8484     \n", 'yellow', attrs=['bold']))
+    print('-' * 70)
 
-
-
+# Function to display the application options and open selected sites
 def app():
-    print("\nHello! welcome to site opener...\nPress(1) for Youtube")
-    print("Press(2) for Google meet")
-    print("Press(3) for Gmail")
+    print("\nHello! Welcome to Site Opener...\n")
+    print("Press (1) for YouTube")
+    print("Press (2) for Google Meet")
+    print("Press (3) for Gmail")
 
     num = input("Enter the command: ")
 
     if num == '1':
         wb.open('https://youtube.com/', new=1)
-        print((colored("\nSite opened in background!", 'green')))
+        print(colored("\nSite opened in background!", 'green'))
     elif num == '2':
         wb.open('https://meet.google.com/', new=1)
-        print((colored("\nSite opened in background!", 'green')))
-    
+        print(colored("\nSite opened in background!", 'green'))
     elif num == '3':
         wb.open('https://accounts.google.com/b/0/AddMailService', new=1)
-        print((colored("\nSite opened in background!", 'green'))
-    
+        print(colored("\nSite opened in background!", 'green'))
     else:
-        print((colored("\nPlease enter numbers from instructions\n", 'red')))
+        print(colored("\nPlease enter valid numbers from instructions\n", 'red'))
 
+# Function to handle exiting or rerunning the program
 def exzt():
-    key = input('Thanks for using the code! enter (y) to run again or any key to quit: ')
+    key = input('Thanks for using the code! Enter (y) to run again or any key to quit: ')
     
-    if key=='y' or key=='Y':
+    if key.lower() == 'y':
         runloop()
     else:
         sys.exit()
 
-header()
-app()
-exzt()
+# Main loop to keep the program running
+def runloop():
+    while True:
+        clear_screen()
+        header()
+        app()
+        exzt()
+
+# Run the program
+if __name__ == "__main__":
+    clear_screen()
+    runloop()
